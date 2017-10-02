cecea26dc78484d4d13f76d813cc261ef05a7e66
Alexander-Serov <a.s.serov@gmail.com>
2017-10-02 12:43:08 +0200

    Small changes

---
+++ load_color_scheme.m
@@ -0,0 +1,132 @@
+%% Define standard colors that I use for plots
+
+
+
+%%%% ===== The main color scheme =====
+my_colors = struct;
+
+
+%% White
+my_colors(1).White = [255, 255, 255]/255;
+
+
+
+%% Black
+my_colors(1).Black = [35.04, 31.31, 32.15]/255;
+
+
+
+%% Light green
+my_colors(1).LightGreen = [202.39, 222.52, 121.66]/255;
+
+
+
+%% Dark olive
+my_colors(1).DarkOlive = [61.77, 61.12, 45.04]/255;
+
+
+
+%% Green
+my_colors(1).Green = [146.85, 197.19, 61.7]/255;
+my_colors(5).Green = [111, 148, 0]/255;
+
+
+
+
+%% Cold brown
+my_colors(1).ColdBrown = [113.16, 104.26, 90.1]/255;
+
+
+
+%% Orange
+my_colors(1).Orange = [242.19, 103.91, 33.59]/255;
+
+
+
+%% Olive
+my_colors(1).Olive = [143.66, 147.73, 101.33]/255;
+
+
+
+%% Warm brown
+my_colors(1).WarmBrown = [149.43, 107.82, 67.56]/255;
+
+
+
+%% Light orange
+my_colors(1).LightOrange = [249.22, 159.52, 37.46]/255;
+
+
+
+%%%% ===== The standard color scheme =====
+standard_colors = struct;
+
+
+
+%% Dark red
+standard_colors(1).DarkRed = [191.28, 32.11, 37.68]/255;
+
+
+
+%% Red
+standard_colors(1).Red = [237.41, 32.25, 36.21]/255;
+
+
+
+%% Dark yellow
+standard_colors(1).DarkYellow = [252.91, 191.76, 16.47]/255;
+
+
+
+%% Yellow
+standard_colors(1).Yellow = [246.32, 235.44, 19.67]/255;
+
+
+
+%% LightGreen
+standard_colors(1).LightGreen = [147.64, 201.39, 83.64]/255;
+
+
+
+%% Dark green
+standard_colors(1).DarkGreen = [0, 176, 79.02]/255;
+
+
+
+%% Light blue
+standard_colors(1).LightBlue = [41.13, 173.28, 227.69]/255;
+
+
+
+%% Deep blue
+standard_colors(1).DeepBlue = [25.7, 112.39, 184.57]/255;
+
+
+
+%% Deep purple
+standard_colors(1).DeepPurple = [31.49, 41.18, 92.53]/255;
+
+
+
+%% Purple
+standard_colors(1).Purple = [111.29, 57.08, 149.74]/255;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
